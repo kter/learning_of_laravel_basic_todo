7f40f73eed24a5cb4a53d3af0aebc86da954be50
kter <me@kter.jp>
2019-04-07 22:24:04 +0900

    タスク一覧表示

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,11 @@
 * タスクダッシュボード表示
 */
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
 
 /**
