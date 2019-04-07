f8911640d5a8ff6c1f4ccb005f267584ee1378b4
kter <me@kter.jp>
2019-04-07 22:05:24 +0900

    valdiator

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,7 +22,15 @@ Route::get('/', function () {
  * タスク追加
  */
 Route::post('/task', function (Request $request) {
-    //
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+        ->withInput()
+        ->withErrors($validator);
+    }
 });
 
 /**
